05196309058ecdeac167e1b501ee0f0dd962d2ce
nguyenletan <nguyenletan@gmail.com>
2022-11-20 00:56:32 +1100

    added chart and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,3 @@
-import Link from 'next/link'
-
-import { Container } from '@/components/Container'
-import { Logo } from '@/components/Logo'
-import { NavLink } from '@/components/NavLink'
-
-
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
 const navigation = {
